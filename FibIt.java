0c85a02b9c54f1c685da8e23a7d1387d45f7ad91
Runeblaze <facss_carpool_service@unc.edu>
2019-04-16 13:39:40 -0400

    made more fun:

---
+++ FibIt.java
@@ -0,0 +1,26 @@
+class FibIt {
+    public static void main(String[] args) {
+        int[] fib = new int[40];
+        fib[0] = 1;
+        fib[1] = 1;
+        int i = 2;
+        while (i < 30) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+            i = i + 1;
+            int z = 100;
+            int p = 233;
+
+            int z_ = 100;
+            int p_ = 233;
+            int i_ = 10;
+            int askjdfha  = 1923841;
+            int aslkdjfhal = 1983;
+        }
+
+        int n = 0;
+        while (n < 30) {
+            System.out.println(fib[n]);
+            n = n + 1;
+        }
+    }
+}
